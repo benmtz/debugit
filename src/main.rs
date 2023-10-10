f6c506ed7e625f691e963d32806ed5c705146022
Ben <ben.mertz.dev@gmail.com>
2023-10-10 16:40:31 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::env::{set_var, args};
-use std::process::Command;
+
+use std::{thread, time};
 
 fn main() {
-    set_var("GIT_TRACE", "1");
-    set_var("GIT_CURL_VERBOSE", "1");
 
-    let received_args = args().collect::<Vec<String>>();
-    Command::new("git").args(&received_args[1..]).spawn().expect("Command failed");
+    let ten_millis = time::Duration::from_millis(100000000);
+    let now = time::Instant::now();
+
+    thread::sleep(ten_millis);
+    
 }
